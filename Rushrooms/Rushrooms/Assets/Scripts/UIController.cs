8dd03f6ae65a38f30465305c3950489e9afed804
Alex-xpuma <alex.puma1225@gmail.com>
2024-05-14 20:01:40 +0300

    without bugs

--- Rushrooms/Rushrooms/Assets/Scripts/UIController.cs
+++ Rushrooms/Rushrooms/Assets/Scripts/UIController.cs
@@ -7,12 +7,11 @@ using TMPro;
 public class UIController : MonoBehaviour
 {
     Player player;
-    //Text distanceText;
+    [SerializeField] TextMeshProUGUI distanceText;
 
     private void Awake()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
-        //distanceText = GameObject.Find("DistanceText").GetComponent<Text>();
     }
     // Start is called before the first frame update
     void Start()
@@ -23,7 +22,7 @@ public class UIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*int distance = Mathf.FloorToInt(player.distance);
-        distanceText.text = distance + " m";*/
+        int distance = Mathf.FloorToInt(player.distance);
+        distanceText.text = distance + " m";
     }
 }
